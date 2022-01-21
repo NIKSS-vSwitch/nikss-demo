f9dece5e18bbf59726b24aa229afc87f9ca1c284
Jan Palimąka <tatry@users.noreply.github.com>
2022-01-21 15:05:51 +0100

    Fix deployment

--- Makefile
+++ Makefile
@@ -4,17 +4,20 @@ start:
 	@./scripts/start-infra.sh
 
 compile:
-	@make -f ${P4C}/backends/ebpf/runtime/kernel.mk BPFOBJ=out.o P4FILE=demo.p4 P4C="p4c-ebpf --arch psa" ARGS="-DBTF -DPSA_PORT_RECIRCULATE=2"
+	@make -f ${P4C_REPO}/backends/ebpf/runtime/kernel.mk BPFOBJ=out.o P4FILE=demo.p4 ARGS="-DPSA_PORT_RECIRCULATE=2" psa
 
 deploy:
-	@ip netns exec switch psabpf-ctl pipeline load id 1 out.o
-	@ip netns exec switch psabpf-ctl pipeline add-port id 1 eth0
-	@ip netns exec switch psabpf-ctl pipeline add-port id 1 eth1
-	@ip netns exec switch psabpf-ctl pipeline add-port id 1 eth2
+	@sudo nsenter --net=/var/run/netns/switch psabpf-ctl pipeline load id 1 out.o
+	@echo "Adding ports to switch..."
+	@sudo nsenter --net=/var/run/netns/switch psabpf-ctl pipeline add-port id 1 psa_recirc
+	@sudo nsenter --net=/var/run/netns/switch psabpf-ctl pipeline add-port id 1 eth0
+	@sudo nsenter --net=/var/run/netns/switch psabpf-ctl pipeline add-port id 1 eth1
+	@sudo nsenter --net=/var/run/netns/switch psabpf-ctl pipeline add-port id 1 eth2
+	@echo "Ports successfully added"
 
 clean:
-	@ip netns exec switch psabpf-ctl pipeline unload id 1
-	@./scripts/stop-infra.sh
-	@rm out.c out.o
+	@sudo nsenter --net=/var/run/netns/switch psabpf-ctl pipeline unload id 1 || true
+	@./scripts/stop-infra.sh || true
+	@rm out.* || true
 
 .PHONY: start compile deploy clean
--- scripts/start-infra.sh
+++ scripts/start-infra.sh
@@ -11,10 +11,9 @@ sudo ip netns add server1
 echo "Creating netns for server2"
 sudo ip netns add server2
 
+sudo ip -n switch link add name psa_recirc type dummy
 sudo ip -n client link add eth0 type veth peer name eth0 netns switch
-
 sudo ip -n server1 link add eth0 type veth peer name eth1 netns switch
-
 sudo ip -n server2 link add eth0 type veth peer name eth2 netns switch
 
 sudo ip netns exec client ip link set lo up
@@ -22,14 +21,19 @@ sudo ip netns exec server1 ip link set lo up
 sudo ip netns exec server2 ip link set lo up
 sudo ip netns exec switch ip link set lo up
 
-sudo ip netns exec client ip add add 10.0.0.1/24 dev eth0
-sudo ip netns exec server1 ip add add 17.0.0.1/24 dev eth0
-sudo ip netns exec server2 ip add add 17.0.0.2/24 dev eth0
+sudo ip netns exec client ip addr add 10.0.0.1/24 dev eth0
+sudo ip netns exec server1 ip addr add 17.0.0.1/24 dev eth0
+sudo ip netns exec server2 ip addr add 17.0.0.2/24 dev eth0
 
 sudo ip netns exec client ip link set eth0 up
 sudo ip netns exec server1 ip link set eth0 up
 sudo ip netns exec server2 ip link set eth0 up
 
+sudo ip netns exec client ip route add default via 10.0.0.254 dev eth0
+sudo ip netns exec server1 ip route add default via 17.0.0.254 dev eth0
+sudo ip netns exec server2 ip route add default via 17.0.0.254 dev eth0
+
 sudo ip netns exec switch ip link set eth0 up
 sudo ip netns exec switch ip link set eth1 up
 sudo ip netns exec switch ip link set eth2 up
+sudo ip netns exec switch ip link set psa_recirc up
